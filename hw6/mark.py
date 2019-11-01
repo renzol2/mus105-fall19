a7eb0d76b6c9132c05ca9261c6ab9d222b91389a
Renzo Ledesma <renzol2@illinois.edu>
2019-11-01 13:52:12 -0500

    finished mark

--- hw6/mark.py
+++ hw6/mark.py
@@ -1,4 +1,4 @@
-###############################################################################
+########################################
 
 from enum import IntEnum
 
@@ -9,7 +9,7 @@ ORNAMENT = 2 << 8
 TEMPORAL = 3 << 8
 
 
-## The Mark class inherits from IntEnum to enumerate various types
+# The Mark class inherits from IntEnum to enumerate various types
 # of notation marks that appear in musical scores. Each Mark enum is
 # a 16 bit value with the format 'ggggggggrrrrrrrr', where:
 # @code
@@ -41,14 +41,48 @@ TEMPORAL = 3 << 8
 #  https://en.wikipedia.org/wiki/Articulation_(music)
 #  https://en.wikipedia.org/wiki/Ornament_(music)
 class Mark (IntEnum):
-    # Create enums here...
+    # TEMPORAL
+    FERMATA = TEMPORAL + 0
+    ACCEL = TEMPORAL + 1
+    DEACCEL = TEMPORAL + 2
 
-    ## Returns the mark's rank number from the lower eight
+    # ORNAMENTS
+    TRILL = ORNAMENT + 0
+    MORDENT = ORNAMENT + 1
+    TURN = ORNAMENT + 2
+
+    # ARTICULATIONS
+    TENUTO = ARTICULATION + 0
+    DETATCHED = ARTICULATION + 1
+    STACCATO = ARTICULATION + 2
+    STACCATISSIMO = ARTICULATION + 3
+    ACCENT = ARTICULATION + 4
+    MARCATO = ARTICULATION + 5
+
+    # DYNAMICS
+    NIENTE = DYNAMIC + 0
+    PPPP = DYNAMIC + 1
+    PPP = DYNAMIC + 2
+    PP = DYNAMIC + 3
+    P = DYNAMIC + 4
+    MP = DYNAMIC + 5
+    MF = DYNAMIC + 6
+    F = DYNAMIC + 7
+    FF = DYNAMIC + 8
+    FFF = DYNAMIC + 9
+    FFFF = DYNAMIC + 10
+    SFZ = DYNAMIC + 11
+    CRESCENDO = DYNAMIC + 12
+    CRESCENDO_END = DYNAMIC + 13
+    DECRESCENDO = DYNAMIC + 14
+    DECRESCENDO_END = DYNAMIC + 15
+
+    # Returns the mark's rank number from the lower eight
     # bits of the enum.
     def rank(self):
-        pass
+        return self.value & 0b11111111
 
-    ## Returns the mark's group number from the upper eight
+    # Returns the mark's group number from the upper eight
     # bits of the enum.
     def group(self):
-       pass
+        return self.value & 0b1111111100000000
