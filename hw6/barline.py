47920fd90578fc23668aecd2deba267f19e66e1d
Renzo Ledesma <renzol2@illinois.edu>
2019-11-01 13:02:01 -0500

    finished enums

--- hw6/barline.py
+++ hw6/barline.py
@@ -2,12 +2,22 @@
 
 from enum import Enum, auto
 
-## A barline represents the right or left side delimiter of a bar.  The set
+
+# A barline represents the right or left side delimiter of a bar.  The set
 # of bar lines are: STANDARD, DOTTED, DASHED, TICKED, SHORT, HEAVY,
 # INTERIOR_DOUBLE, FINAL_DOUBLE, LEFT_REPEAT, RIGHT_REPEAT, MIDDLE_REPEAT.
 # The enum values do not matter, you can use enum.auto() to assign them.
 # See: https://en.wikipedia.org/wiki/Bar_(music)
 class Barline (Enum):
-    # Create enums here...
-    pass
+    STANDARD = auto()
+    DOTTED = auto()
+    DASHED = auto()
+    TICKED = auto()
+    SHORT = auto()
+    HEAVY = auto()
+    INTERIOR_DOUBLE = auto()
+    FINAL_DOUBLE = auto()
+    LEFT_REPEAT = auto()
+    RIGHT_REPEAT = auto()
+    MIDDLE_REPEAT = auto()
 
