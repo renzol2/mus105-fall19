b189dd2101f0487193e6dba7dce22c10038b443a
Renzo Ledesma <renzol2@illinois.edu>
2019-11-01 13:37:33 -0500

    finished mode

--- hw6/mode.py
+++ hw6/mode.py
@@ -1,24 +1,32 @@
-###############################################################################
+########################################
 
 from enum import IntEnum
 
 
-## An enumeration of all the 7-tone modes: MAJOR, MINOR, IONIAN, DORIAN,
+# An enumeration of all the 7-tone modes: MAJOR, MINOR, IONIAN, DORIAN,
 # PHRYGIAN, LYDIAN, MIXOLYDIAN, AEOLIAN, and LOCRIAN. The value for each mode
 # should be a number index 0-6 representing its diatonic scale degree C-B.
 # For example, MAJOR will be 0 (C), MINOR is 5 (A) and Dorian is 1 (D).
 # Define IONIAN and AEOLIAN as synonyms for MAJOR and MINOR by assigning
 # them their corresponding enum value.
 class Mode (IntEnum):
-    # Create enums here...
+    MAJOR = 0
+    DORIAN = 1
+    PHRYGIAN = 2
+    LYDIAN = 3
+    MIXOLYDIAN = 4
+    MINOR = 5
+    LOCRIAN = 6
 
+    IONIAN = MAJOR
+    AEOLIAN = MINOR
 
-    ## Returns only the first three characters of the mode's name.
+    # Returns only the first three characters of the mode's name.
     def short_name(self):
-        pass
+        return self.name[0:3]
 
-    ## Returns the integer degree number representing the starting
+    # Returns the integer degree number representing the starting
     # scale degree of the mode. Thus IONIAN and MAJOR are 0,
     # DORIAN is 1, LOCRIAN is 6 and so on.
     def tonic_degree(self):
-        pass
+        return int(self)
